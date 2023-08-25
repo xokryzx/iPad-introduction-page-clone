6018df2bcd540f359ed8232c3442553ae8ba668f
xokryzx <xokryzx@gmail.com>
2023-08-26 04:19:36 +0900

    ✨ Add a search bar to the menu

--- js/header.js
+++ js/header.js
@@ -20,3 +20,49 @@ $cartContainer.addEventListener('click', (event) => {
   event.stopPropagation();
 });
 window.addEventListener('click', hideCart);
+
+const $header = document.querySelector('header');
+const $menuList = [...$header.querySelectorAll('.menu-list')];
+const $searchButton = $header.querySelector('.search-button');
+const $searchContainer = $header.querySelector('.search-container');
+const $searchShadow = $searchContainer.querySelector('.shadow');
+const $searchInput = $searchContainer.querySelector('.search-input');
+const $searchCloser = $searchContainer.querySelector('.search-closer');
+const $searchDelay = [...$searchContainer.querySelectorAll('.search-menu__list')];
+
+function showSearch() {
+  $header.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+
+  $menuList.reverse().forEach((element, index) => {
+    element.style.transitionDelay = (index * 0.4) / $menuList.length + 's';
+  });
+
+  $searchDelay.forEach((element, index) => {
+    element.style.transitionDelay = (index * 0.4) / $searchDelay.length + 's';
+  });
+
+  setTimeout(() => {
+    $searchInput.focus();
+  }, 600);
+}
+
+function hideSearch() {
+  $header.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+
+  $menuList.reverse().forEach((element, index) => {
+    element.style.transitionDelay = (index * 0.4) / $menuList.length + 's';
+  });
+
+  $searchDelay.reverse().forEach((element, index) => {
+    element.style.transitionDelay = (index * 0.4) / $searchDelay.length + 's';
+  });
+  $searchDelay.reverse();
+
+  $searchInput.value = '';
+}
+
+$searchButton.addEventListener('click', showSearch);
+$searchShadow.addEventListener('click', hideSearch);
+$searchCloser.addEventListener('click', hideSearch);
