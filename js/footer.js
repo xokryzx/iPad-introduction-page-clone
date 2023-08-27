6dc8dabac34552253fec0e2fde65b8a56a6aef52
xokryzx <xokryzx@gmail.com>
2023-08-28 01:24:10 +0900

    ✨ Add a navigation section to the footer component

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+import navigations from './data/navigations.js';
+
+const $footerNav = document.querySelector('.footer-nav');
+navigations.forEach((nav) => {
+  const $map = document.createElement('div');
+  $map.classList.add('map');
+
+  let mapList = '';
+  nav.maps.forEach((map) => {
+    mapList += `
+      <li class="map-list">
+        <a href="${map.url}" class="map-link">${map.name}</a>
+      </li>
+    `;
+  });
+
+  $map.innerHTML = `
+    <div class="map-title">${nav.title}</div>
+    <ul>${mapList}</ul>
+  `;
+  $footerNav.append($map);
+});
